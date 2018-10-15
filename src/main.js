81135a8aba57c2cf7f8c4cdfd69f8f38f72bb7dc
志田守 <mamoru.bit0926@gmail.com>
2018-10-15 16:32:08 +0900

    finish

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCFBT1OF112MzBV_naIUxznTJuCjnpvduU",
+  authDomain: "criptodash-5f50c.firebaseapp.com",
+  databaseURL: "https://criptodash-5f50c.firebaseio.com",
+  projectId: "criptodash-5f50c",
+  storageBucket: "criptodash-5f50c.appspot.com",
+  messagingSenderId: "379460309493"
+};
+firebase.initializeApp(config);
